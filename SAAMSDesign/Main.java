71811b97ac6d33f7e264332949afa8eb366d3ae3
CptFantabulous <fergus120@hotmail.co.uk>
2018-03-05 10:46:34 +0000

    Intial Upload of the SAAMDesing Folder

---
+++ SAAMSDesign/Main.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * The Main class.
+ *
+ * The principal component is the usual main method required by Java application to launch the application.
+ *
+ * Instantiates the databases.
+ * Instantiates and shows all the system interfaces as Frames.
+ * @stereotype control
+ */
+public class Main {
+
+
+/**
+ * Launch SAAMS.
+ */
+
+public static void main(String[] args) {
+    // Instantiate databases
+    // Instantiate and show all interfaces as Frames
+  }
+
+}
