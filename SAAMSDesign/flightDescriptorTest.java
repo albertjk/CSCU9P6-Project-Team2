2237855076d879d1d41343228b665581cd52c8b3
CptFantabulous <fergus120@hotmail.co.uk>
2018-03-07 17:22:37 +0000

    revised FDTest
    basically cut out the unneeded lines and added test methods.
    
    However you still need to move where the variables are declared to outside of set up and fill out the test methods

--- SAAMSDesign/flightDescriptorTest.java
+++ SAAMSDesign/flightDescriptorTest.java
@@ -1,29 +1,39 @@
 import java.util.ArrayList;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class flightDescriptorTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
-		ArrayList PassangerList = new ArrayList();
+@Before
+	public static void setUp() {
 		PassengerList pl1 = new PassengerList();
 		
 		Itinerary it1 = new Itinerary("Glasgow", "Stirling", "London");
-		Itinerary it2 = new Itinerary("Stirling", "Callais", "Glasgow");
-		
-		ArrayList testList = new ArrayList(); //to store the return function
-		
-		FlightDescriptor fd1 = new FlightDescriptor("BE111", it1, pl1 );
 
 	
 		for(int i = 0; i < 10; i++) { //adds 10 passengers called mark to the ArrayList		
 			
 			PassengerDetails test = new PassengerDetails("Mark"); //Creates a new test object called mark
 				pl1.addPassenger(test); //adds and new passenger to the array
-			
-				testList = pl1.getPassengerList(); //add the contents of the passList1 to a testable array
 		}
+		FlightDescriptor fd1 = new FlightDescriptor("BE111", it1, pl1 );
 		
 	}
 
+@Test
+public void testItinerary()
+{
+	
+}
+@Test
+public void testFlightNumber()
+{
+	
+}
+
+@Test
+public void testPassengerList()
+{
+	
+}
 }
