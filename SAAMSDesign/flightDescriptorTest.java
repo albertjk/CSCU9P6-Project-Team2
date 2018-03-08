eeb8690971aa2c3ac75047b144aea2b0e390686d
2112688 <34160577+2112688@users.noreply.github.com>
2018-03-08 15:56:45 +0000

    Add files via upload
    just a few issues with running this. Eclipse thinks should be static, but overall initialization errors. moved things around but not much works (could be my system. its being a bit funny atm about code i wrote prior that SHOULD work)

--- SAAMSDesign/flightDescriptorTest.java
+++ SAAMSDesign/flightDescriptorTest.java
@@ -4,36 +4,45 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class flightDescriptorTest {
-@Before
-	public static void setUp() {
-		PassengerList pl1 = new PassengerList();
-		
-		Itinerary it1 = new Itinerary("Glasgow", "Stirling", "London");
+	
+	static PassengerList pl1 = new PassengerList();
+	static Itinerary it1 = new Itinerary("Glasgow", "Stirling", "London");
 
 	
+
+@Before
+	public  void setUp() {
+
 		for(int i = 0; i < 10; i++) { //adds 10 passengers called mark to the ArrayList		
 			
 			PassengerDetails test = new PassengerDetails("Mark"); //Creates a new test object called mark
 				pl1.addPassenger(test); //adds and new passenger to the array
 		}
-		FlightDescriptor fd1 = new FlightDescriptor("BE111", it1, pl1 );
 		
 	}
+
+	FlightDescriptor fd1 = new FlightDescriptor("BE111", it1, pl1 );
+
+
 
 @Test
 public void testItinerary()
 {
+	it1.getFrom();
+	it1.getNext();
+	it1.getTo();
 	
 }
 @Test
 public void testFlightNumber()
 {
+	fd1.toString(); // eh
 	
 }
 
 @Test
 public void testPassengerList()
 {
-	
+	pl1.getPassengerList();
 }
 }
