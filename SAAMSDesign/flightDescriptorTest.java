e2d1a6a904fac77e255a8ff6d7f565ba5bc8ac88
2112688 <34160577+2112688@users.noreply.github.com>
2018-03-09 22:32:20 +0000

    Add files via upload

--- SAAMSDesign/flightDescriptorTest.java
+++ SAAMSDesign/flightDescriptorTest.java
@@ -5,37 +5,35 @@ import org.junit.Test;
 
 public class flightDescriptorTest {
 	
-	PassengerList pl1 = new PassengerList();
-	Itinerary it1 = new Itinerary("Glasgow", "Stirling", "London");
-	FlightDescriptor fd1;
-	
+	static PassengerList pl1 = new PassengerList();
+	static Itinerary it1 = new Itinerary("Glasgow", "Stirling", "London");
 
 	
-
 @Before
 	public  void setUp() {
 
-		for(int i = 0; i < 10; i++) { //adds 10 passengers called mark to the ArrayList		
-			
+		for(int i = 0; i < 10; i++) { //adds 10 passengers called mark to the ArrayList			
 			PassengerDetails test = new PassengerDetails("Mark"); //Creates a new test object called mark
 				pl1.addPassenger(test); //adds and new passenger to the array
 		}
-		fd1 = new FlightDescriptor("BE111", it1, pl1);
 	}
 
-
+	FlightDescriptor fd1 = new FlightDescriptor("BE111", it1, pl1 );
 
 @Test
 public void testItinerary()
 {
+	fd1.getIT();	
 }
 @Test
 public void testFlightNumber()
 {
+	fd1.getfCode();
 }
 
 @Test
 public void testPassengerList()
 {
+	fd1.getPassList();
 }
 }
