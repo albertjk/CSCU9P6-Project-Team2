c38fc763c4269f92dca37b374372c94a3ed46c3d
CptFantabulous <fergus120@hotmail.co.uk>
2018-03-12 17:10:04 +0000

    the test for RadarDetect is now done in setUp
    It makes sense since radarDetect is basically the constructor as well.

--- SAAMSDesign/ManagementRecordTest.java
+++ SAAMSDesign/ManagementRecordTest.java
@@ -38,13 +38,8 @@ public class ManagementRecordTest {
 		FD2 = new FlightDescriptor(flightCode2, it2, PL2);
 		mr1 = new ManagementRecord();
 		mr2 = new ManagementRecord();
-	}
-	@Test
-	public void testRadarDetect()
-	{
 		mr1.radarDetect(FD1);
 		mr2.radarDetect(FD2);
-		
 		int counter = 1;
 		for(int i = 0;i < MR.length;i++)//So this fills the array with instances of mr1 and 2. At this point they all have the status of FREE
 		{
@@ -70,6 +65,34 @@ public class ManagementRecordTest {
 			MR[i].radarDetect(FD1);
 		}
 	}
+	//@Test
+	//public void testRadarDetect()
+	//{	
+		//int counter = 1;
+		//for(int i = 0;i < MR.length;i++)//So this fills the array with instances of mr1 and 2. At this point they all have the status of FREE
+		//{
+		//	if(counter == 2)
+		//	{
+		//	MR[i] = mr1;
+		//	counter++;
+		//	}
+			//else
+			//{
+		//		MR[i] = mr2;
+			//	counter++;
+			//}
+		//}
+		//for(int i = 0; i < MR.length;i++)//This loop then steps through and changes the status of some of the entries.
+		//{
+		//		MR[i].setStatus(0);	
+		//}
+		//Now that half of the entries are set to something other than free we can test the rejection. We should expect 5 error messages to be printed out on screen. 
+		//for(int i = 0; i < MR.length;i++)
+		//{
+		//	System.out.println("Current status of MR " + i + " " + MR[i].getStatus());
+		//	MR[i].radarDetect(FD1);
+		//}
+//	}
 	
 	@Test
 	public void testRadarLostContact()
