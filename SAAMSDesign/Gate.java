a63ea1f3abab9557ebd137c576e9392548a0efe1
albertjk <alj00025@students.stir.ac.uk>
2018-03-18 11:46:44 +0000

    Added Observers/Observables, subscribed to the models

--- SAAMSDesign/Gate.java
+++ SAAMSDesign/Gate.java
@@ -16,63 +16,63 @@
  */
 public class Gate {
 	
-/**
- *  Status code representing the situation when the gate is currently allocated to no aircraft.
- */
+	/**
+	* Status code representing the situation when the gate is currently allocated to no aircraft.
+	*/
 	public static final int FREE = 0;
 
-/**
- *  Status code representing the situation when the gate has been allocated to an aircraft that has just landed, but the aircraft has not yet docked at the gate.
- */
-  public static final int RESERVED = 1;
+	/**
+	* Status code representing the situation when the gate has been allocated to an aircraft that has just landed, but the aircraft has not yet docked at the gate.
+	*/
+	public static final int RESERVED = 1;
 
-/**
- *  Status code representing the situation when an aircraft is currently at the gate - either unloading passengers, being cleaned and maintained, loading new passengers or finished loading but no permission to taxi to the runway has yet been granted.
- */
-  public static final int OCCUPIED = 2;
+	/**
+	* Status code representing the situation when an aircraft is currently at the gate - either unloading passengers, being cleaned and maintained, loading new passengers or finished loading but no permission to taxi to the runway has yet been granted.
+	*/
+	public static final int OCCUPIED = 2;
 
-/**
- *  Holds the code indicating the current status of this gate.
- */
-  private int status = FREE;
+	/**
+	* Holds the code indicating the current status of this gate.
+	*/
+	private int status = FREE;
 
-/**
- *  If the gate is reserved or occupied, the mCode of the MR of the aircraft which is expected/present.
- */
-  private int mCode;
+	/**
+	* If the gate is reserved or occupied, the mCode of the MR of the aircraft which is expected/present.
+	*/
+	private int mCode;
   
-  /**
-   *  Return the status code for this gate.
-   */
-  public int getStatus(){
-	  return this.status;
-  }
+	/**
+	* Return the status code for this gate.
+	*/
+	public int getStatus(){
+		return this.status;
+	}
 
-/**
- *  The gate has been allocated to the given aircraft, identified by mCode: Change status from FREE to RESERVED and note the mCode.
-  * @preconditions Status must be Free*/
-  public void allocate(int mCode){
-	  if(this.status == FREE) {
-		  this.status = RESERVED;
-		  this.mCode = mCode;
-	  }
-  }
+	/**
+	* The gate has been allocated to the given aircraft, identified by mCode: Change status from FREE to RESERVED and note the mCode.
+	* @preconditions Status must be Free*/
+	public void allocate(int mCode){
+		if(this.status == FREE) {
+			this.status = RESERVED;
+			this.mCode = mCode;
+		}
+	}
 
-/**
- *  Change gate status from RESERVED to OCCUPIED to indicate that aircraft has now docked.
-  * @preconditions Status must be Reserved*/
-  public void docked(){
-	  if(this.status == RESERVED) {
-		  this.status = OCCUPIED;
-	  }
-  }
+	/**
+	* Change gate status from RESERVED to OCCUPIED to indicate that aircraft has now docked.
+	* @preconditions Status must be Reserved*/
+	public void docked(){
+		if(this.status == RESERVED) {
+			this.status = OCCUPIED;
+		}
+	}
 
-/**
- *  Change status from OCCUPIED to FREE as the docked aircraft has now departed.
-  * @preconditions Status must be Occupied*/
-  public void departed(){
-	  if(this.status == OCCUPIED) {
-		  this.status = FREE;
-	  }
-  }
+	/**
+	* Change status from OCCUPIED to FREE as the docked aircraft has now departed.
+	* @preconditions Status must be Occupied*/
+	public void departed(){
+		if(this.status == OCCUPIED) {
+			this.status = FREE;
+		}
+  	}
 }
